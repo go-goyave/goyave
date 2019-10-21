182da89b6d724c6ce63509035b6cca03547e8636
SystemGlitch <jeremy.la@outlook.fr>
2019-10-21 12:19:23 +0200

    Setup docs

---
+++ docs/assets/js/6.b71de841.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{9:function(n,w,o){}}]);
---
+++ docs_src/src/.vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    title: 'Goyave Documentation',
+    description: 'The documentation for the Goyave web framework',
+    dest: '../docs',
+    base: '/goyave/'
+}

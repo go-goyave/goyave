f6968d7eb2f8b58b4f4e946d5d941e930e3d0f2a
SystemGlitch <jeremy.la@outlook.fr>
2019-10-29 17:28:24 +0100

    Add integer validation rule

--- validation/rules.go
+++ validation/rules.go
@@ -46,7 +46,39 @@ func validateNumeric(field string, value interface{}, parameters []string, form 
 	}
 }
 
-// integer
+func validateInteger(field string, value interface{}, parameters []string, form map[string]interface{}) bool {
+	rv := reflect.ValueOf(value)
+	kind := rv.Kind().String()
+	switch {
+	case strings.HasPrefix(kind, "int"), strings.HasPrefix(kind, "uint") && kind != "uintptr":
+		return true
+	case strings.HasPrefix(kind, "float"):
+		if kind == "float64" {
+			val, _ := value.(float64)
+			if val-float64(int(val)) > 0 {
+				return false
+			}
+			form[field] = int(val)
+			return true
+		}
+
+		val, _ := value.(float32)
+		if val-float32(int(val)) > 0 {
+			return false
+		}
+		form[field] = int(val)
+		return true
+	case kind == "string":
+		intVal, err := strconv.Atoi(value.(string))
+		if err == nil {
+			form[field] = intVal
+		}
+		return err == nil
+	default:
+		return false
+	}
+}
+
 // between
 // min
 // max
--- validation/rules_test.go
+++ validation/rules_test.go
@@ -37,6 +37,39 @@ func TestValidateNumericConvertString(t *testing.T) {
 	assert.Equal(t, float64(2), form3["field"])
 }
 
+func TestValidateInteger(t *testing.T) {
+	assert.True(t, validateInteger("field", 1, []string{}, map[string]interface{}{"field": 1}))
+	assert.True(t, validateInteger("field", float64(2), []string{}, map[string]interface{}{"field": float64(2)}))
+	assert.True(t, validateInteger("field", float32(3), []string{}, map[string]interface{}{"field": float32(3)}))
+	assert.True(t, validateInteger("field", uint(1), []string{}, map[string]interface{}{"field": uint(1)}))
+	assert.True(t, validateInteger("field", uint8(1), []string{}, map[string]interface{}{"field": uint8(1)}))
+	assert.True(t, validateInteger("field", uint16(1), []string{}, map[string]interface{}{"field": uint16(1)}))
+	assert.True(t, validateInteger("field", "2", []string{}, map[string]interface{}{"field": "2"}))
+	assert.True(t, validateInteger("field", "-1", []string{}, map[string]interface{}{"field": "-1"}))
+	assert.False(t, validateInteger("field", 2.2, []string{}, map[string]interface{}{"field": 2.2}))
+	assert.False(t, validateInteger("field", float32(3.2), []string{}, map[string]interface{}{"field": float32(3.2)}))
+	assert.False(t, validateInteger("field", "1.2", []string{}, map[string]interface{}{"field": "1.2"}))
+	assert.False(t, validateInteger("field", "-1.3", []string{}, map[string]interface{}{"field": "1.3"}))
+	assert.False(t, validateInteger("field", uintptr(1), []string{}, map[string]interface{}{"field": uintptr(1)}))
+	assert.False(t, validateInteger("field", []string{}, []string{}, map[string]interface{}{"field": []string{}}))
+	assert.False(t, validateInteger("field", map[string]string{}, []string{}, map[string]interface{}{"field": map[string]string{}}))
+	assert.False(t, validateInteger("field", "test", []string{}, map[string]interface{}{"field": "test"}))
+}
+
+func TestValidateIntegerConvert(t *testing.T) {
+	form1 := map[string]interface{}{"field": "1"}
+	validateInteger("field", form1["field"], []string{}, form1)
+	assert.Equal(t, 1, form1["field"])
+
+	form2 := map[string]interface{}{"field": "-2"}
+	validateInteger("field", form2["field"], []string{}, form2)
+	assert.Equal(t, -2, form2["field"])
+
+	form3 := map[string]interface{}{"field": float64(3)}
+	validateInteger("field", form3["field"], []string{}, form3)
+	assert.Equal(t, 3, form3["field"])
+}
+
 func TestValidateString(t *testing.T) {
 	assert.True(t, validateString("field", "string", []string{}, map[string]interface{}{}))
 	assert.False(t, validateString("field", 2, []string{}, map[string]interface{}{}))

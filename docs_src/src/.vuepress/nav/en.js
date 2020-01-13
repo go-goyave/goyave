f341abf1ce0412f0a6dfb333d832c6d7ebdae6d2
SystemGlitch <jeremy.la@outlook.fr>
2020-01-13 12:15:29 +0100

    Add GoDoc links

--- docs_src/src/.vuepress/nav/en.js
+++ docs_src/src/.vuepress/nav/en.js
@@ -2,6 +2,10 @@ module.exports = [
     {
       text: "Guide",
       link: "/guide/"
+    },
+    {
+      text: 'GoDoc',
+      link: 'https://godoc.org/github.com/System-Glitch/goyave'
     }
   ];
   

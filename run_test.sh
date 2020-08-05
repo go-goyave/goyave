9fe5342c7280258a415f9256cafcc65f590b7f58
SystemGlitch <jeremy.la@outlook.fr>
2020-08-05 16:32:28 +0200

    Lower db ping time in run_test script

--- run_test.sh
+++ run_test.sh
@@ -28,13 +28,13 @@ while [ $health -ne 0 ]; do
 	mysql -h 127.0.0.1 --protocol=tcp -u goyave --password=secret goyave -e "SHOW TABLES;" >/dev/null 2>&1
 	health=$?
 	((tries++))
-	if [ $tries -gt 50 ]; then
+	if [ $tries -gt 100 ]; then
 		docker stop $container
-		echo -e "\033[31mError: couldn't connect to container database after 50 retries.\033[0m"
+		echo -e "\033[31mError: couldn't connect to container database after 100 retries.\033[0m"
 		exit 2
 	fi
-	echo -e "\033[33mCouldn't connect to container database. Retrying in 10 seconds...\033[0m"
-	sleep 10
+	echo -e "\033[33mCouldn't connect to container database. Retrying in 5 seconds...\033[0m"
+	sleep 5
 done
 
 echo -e "\033[92m\033[1mDatabase ready. Running tests...\033[0m"

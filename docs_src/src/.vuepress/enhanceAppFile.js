f3c13c219696949a7d1e01621ea676923d92f49f
SystemGlitch <jeremy.la@outlook.fr>
2021-03-01 14:26:03 +0100

    Delete docs_src

--- docs_src/src/.vuepress/enhanceAppFile.js
+++
@@ -1,6 +0,0 @@
-import MigrationBanner from './components/MigrationBanner.vue'
-
-export default ({ Vue }) => {
-  // eslint-disable-next-line vue/match-component-file-name
-  Vue.component('MigrationBanner', MigrationBanner)
-}

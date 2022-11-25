4d346afc1db77e25b7c5f2db692f12d6f12d1683
SystemGlitch <jeremy.la@outlook.fr>
2022-11-25 15:05:22 +0100

    Validation: document required validator

--- validation/required.go
+++ validation/required.go
@@ -1,7 +1,15 @@
 package validation
 
+// RequiredValidator the field under validation is required.
+// If a field is absent from the input data, subsequent validators
+// will not be executed.
+//
+// If a field is `nil` and has the `Nullable` validator, this validator passes.
+// As non-nullable fields are removed if they have a `nil` value, this validator
+// doesn't pass if a field is `nil` and doesn't have the `Nullable` validator.
 type RequiredValidator struct{ BaseValidator }
 
+// Validate checks the field under validation satisfies this validator's criteria.
 func (v *RequiredValidator) Validate(ctx *ContextV5) bool {
 	if !ctx.Field.IsNullable() && ctx.Value == nil {
 		return false
@@ -9,17 +17,28 @@ func (v *RequiredValidator) Validate(ctx *ContextV5) bool {
 	return true
 }
 
+// Name returns the string name of the validator.
 func (v *RequiredValidator) Name() string { return "required" }
 
+// Required the field under validation is required.
+// If a field is absent from the input data, subsequent validators
+// will not be executed.
+//
+// If a field is `nil` and has the `Nullable` validator, this validator passes.
+// As non-nullable fields are removed if they have a `nil` value, this validator
+// doesn't pass if a field is `nil` and doesn't have the `Nullable` validator.
 func Required() *RequiredValidator {
 	return &RequiredValidator{}
 }
 
+// RequiredIfValidator is the same as `RequiredValidator` but only applies the behavior
+// described if the specified `Condition` function returns true.
 type RequiredIfValidator struct {
 	RequiredValidator
 	Condition func(*ContextV5) bool
 }
 
+// Validate checks the field under validation satisfies this validator's criteria.
 func (v *RequiredIfValidator) Validate(ctx *ContextV5) bool {
 	if !v.Condition(ctx) {
 		return true
@@ -27,6 +46,8 @@ func (v *RequiredIfValidator) Validate(ctx *ContextV5) bool {
 	return v.RequiredValidator.Validate(ctx)
 }
 
+// RequiredIf is the same as `Required` but only applies the behavior
+// described if the specified condition function returns true.
 func RequiredIf(condition func(*ContextV5) bool) *RequiredIfValidator {
 	return &RequiredIfValidator{Condition: condition}
 }

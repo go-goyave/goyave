5cedf95aa414e94460c3875c840aa48557616b6f
SystemGlitch <jeremy.la@outlook.fr>
2022-11-25 15:58:45 +0100

    Validation: implement UUID validator

---
+++ validation/uuid.go
@@ -0,0 +1,49 @@
+package validation
+
+import (
+	"github.com/google/uuid"
+	"github.com/samber/lo"
+)
+
+// UUIDValidator the field under validation must be a string representing
+// a valid UUID.
+// If one or more `accepterVersions` are provided, the parsed UUID must
+// be a UUID of one of these versions. If none are given, all versions are
+// accepted.
+type UUIDValidator struct {
+	BaseValidator
+	AcceptedVersions []uuid.Version
+}
+
+// Validate checks the field under validation satisfies this validator's criteria.
+func (v *UUIDValidator) Validate(ctx *ContextV5) bool {
+	val, ok := ctx.Value.(string)
+	if !ok {
+		return false
+	}
+	id, err := uuid.Parse(val)
+	if err != nil {
+		return false
+	}
+	if len(v.AcceptedVersions) == 0 {
+		return true
+	}
+	return lo.Contains(v.AcceptedVersions, id.Version())
+}
+
+// Name returns the string name of the validator.
+func (v *UUIDValidator) Name() string { return "uuid" }
+
+// IsType returns true.
+func (v *UUIDValidator) IsType() bool { return true }
+
+// TODO specify accepted versions in validation message?
+
+// UUID the field under validation must be a string representing
+// a valid UUID.
+// If one or more `accepterVersions` are provided, the parsed UUID must
+// be a UUID of one of these versions. If none are given, all versions are
+// accepted.
+func UUID(acceptedVersions ...uuid.Version) *UUIDValidator {
+	return &UUIDValidator{AcceptedVersions: acceptedVersions}
+}

377e2cc9d0de1ec0ff52469214c3115b01a50e5c
SystemGlitch <jeremy.la@outlook.fr>
2022-11-25 15:33:15 +0100

    Validation: implement timezone validator

---
+++ validation/timezone.go
@@ -0,0 +1,54 @@
+package validation
+
+import (
+	"sync"
+	"time"
+)
+
+var timezoneCache = sync.Map{} // TODO May cause trouble testing?
+
+// TimezoneValidator the field under validation must be a valid string
+// reprensentation of a timezone.
+// If validation passes, the value is converted to `*time.Location`
+// using `time.LoadLocation()`.
+// "Local" as an input is not accepted as a valid timezone.
+//
+// As `time.LoadLocation()` can be a slow operation, timezones are cached.
+type TimezoneValidator struct{ BaseValidator }
+
+// Validate checks the field under validation satisfies this validator's criteria.
+func (v *TimezoneValidator) Validate(ctx *ContextV5) bool {
+	tz, ok := ctx.Value.(string)
+	if !ok || tz == "Local" {
+		return false
+	}
+
+	if loc, ok := timezoneCache.Load(tz); ok {
+		ctx.Value = loc
+		return true
+	}
+
+	loc, err := time.LoadLocation(tz)
+	if err != nil {
+		return false
+	}
+
+	timezoneCache.Store(tz, loc)
+	ctx.Value = loc
+	return true
+}
+
+// Name returns the string name of the validator.
+func (v *TimezoneValidator) Name() string { return "timezone" }
+
+// IsType returns true.
+func (v *TimezoneValidator) IsType() bool { return true }
+
+// Timezone the field under validation must be a valid string reprensentation of a timezone.
+// If validation passes, the value is converted to `*time.Location` using `time.LoadLocation()`.
+// "Local" as an input is not accepted as a valid timezone.
+//
+// As `time.LoadLocation()` can be a slow operation, timezones are cached.
+func Timezone() *TimezoneValidator {
+	return &TimezoneValidator{}
+}

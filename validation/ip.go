ee6305d62c50e76e20744a0e51aad3cbd1aea33e
SystemGlitch <jeremy.la@outlook.fr>
2022-11-25 16:15:02 +0100

    Validation: implement IP validator

---
+++ validation/ip.go
@@ -0,0 +1,89 @@
+package validation
+
+import "net"
+
+// IPValidator the field under validation must be a string representing
+// a valid IPv4 or IPv6.
+// If validation passes, the value is converted to `net.IP`.
+type IPValidator struct{ BaseValidator }
+
+// Validate checks the field under validation satisfies this validator's criteria.
+func (v *IPValidator) Validate(ctx *ContextV5) bool {
+	if _, ok := ctx.Value.(net.IP); ok {
+		return true
+	}
+
+	val, ok := ctx.Value.(string)
+	if !ok {
+		return false
+	}
+
+	ip := net.ParseIP(val)
+	if ip == nil {
+		return false
+	}
+
+	ctx.Value = ip
+	return true
+}
+
+// Name returns the string name of the validator.
+func (v *IPValidator) Name() string { return "ip" }
+
+// IsType returns true.
+func (v *IPValidator) IsType() bool { return true }
+
+// IP the field under validation must be a string representing
+// a valid IPv4 or IPv6.
+// If validation passes, the value is converted to `net.IP`.
+func IP() *IPValidator {
+	return &IPValidator{}
+}
+
+//------------------------------
+
+// IPv4Validator the field under validation must be a string representing
+// a valid IPv4.
+// If validation passes, the value is converted to `net.IP`.
+type IPv4Validator struct{ IPValidator }
+
+// Validate checks the field under validation satisfies this validator's criteria.
+func (v *IPv4Validator) Validate(ctx *ContextV5) bool {
+	if !v.IPValidator.Validate(ctx) {
+		return false
+	}
+	return ctx.Value.(net.IP).To4() != nil
+}
+
+// Name returns the string name of the validator.
+func (v *IPv4Validator) Name() string { return "ipv4" }
+
+// IPv4 the field under validation must be a string representing a valid IPv4.
+// If validation passes, the value is converted to `net.IP`.
+func IPv4() *IPv4Validator {
+	return &IPv4Validator{}
+}
+
+//------------------------------
+
+// IPv6Validator the field under validation must be a string representing
+// a valid IPv6.
+// If validation passes, the value is converted to `net.IP`.
+type IPv6Validator struct{ IPValidator }
+
+// Validate checks the field under validation satisfies this validator's criteria.
+func (v *IPv6Validator) Validate(ctx *ContextV5) bool {
+	if !v.IPValidator.Validate(ctx) {
+		return false
+	}
+	return ctx.Value.(net.IP).To4() == nil
+}
+
+// Name returns the string name of the validator.
+func (v *IPv6Validator) Name() string { return "ipv6" }
+
+// IPv6 the field under validation must be a string representing a valid IPv6.
+// If validation passes, the value is converted to `net.IP`.
+func IPv6() *IPv6Validator {
+	return &IPv6Validator{}
+}

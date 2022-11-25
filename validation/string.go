55aee547b6d9d55083b3b016519eeeef5266cd25
SystemGlitch <jeremy.la@outlook.fr>
2022-11-25 15:09:29 +0100

    Validation: document string validator

--- validation/string.go
+++ validation/string.go
@@ -1,15 +1,21 @@
 package validation
 
+// StringValidator the field under validation must be a string.
 type StringValidator struct{ BaseValidator }
 
+// Validate checks the field under validation satisfies this validator's criteria.
 func (v *StringValidator) Validate(ctx *ContextV5) bool {
 	_, ok := ctx.Value.(string)
 	return ok
 }
 
+// Name returns the string name of the validator.
 func (v *StringValidator) Name() string { return "string" }
+
+// IsType returns true.
 func (v *StringValidator) IsType() bool { return true }
 
+// String the field under validation must be a string.
 func String() *StringValidator {
 	return &StringValidator{}
 }

294c0511364dd57708fb9fb2a5308d4f401ea1e9
SystemGlitch <jeremy.la@outlook.fr>
2022-11-25 14:46:24 +0100

    Validation: implement regex validator

---
+++ validation/regex_v5.go
@@ -0,0 +1,32 @@
+package validation
+
+import "regexp"
+
+// RegexValidator the field under validation must be a string matching
+// the specified `*regexp.Regexp`.
+type RegexValidator struct {
+	BaseValidator
+	Regexp *regexp.Regexp
+}
+
+// Validate checks the field under validation satisfies this validator's criteria.
+func (v *RegexValidator) Validate(ctx *ContextV5) bool {
+	val, ok := ctx.Value.(string)
+	return ok && v.Regexp.MatchString(val)
+}
+
+// Name returns the string name of the validator.
+func (v *RegexValidator) Name() string { return "regex" }
+
+// MessagePlaceholders returns the ":regexp" placeholder.
+func (v *RegexValidator) MessagePlaceholders(ctx *ContextV5) []string {
+	return []string{
+		":regexp", v.Regexp.String(),
+	}
+}
+
+// Regex the field under validation must be a string matching
+// the specified `*regexp.Regexp`.
+func Regex(regex *regexp.Regexp) *RegexValidator {
+	return &RegexValidator{Regexp: regex}
+}

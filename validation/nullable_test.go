0ff2f609b1ef44be33f7cd0be85a974e9d65e823
SystemGlitch <jeremy.la@outlook.fr>
2023-04-17 10:44:24 +0200

    Validation: test Nullable

---
+++ validation/nullable_test.go
@@ -0,0 +1,47 @@
+package validation
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNullableValidator(t *testing.T) {
+	t.Run("Constructor", func(t *testing.T) {
+		v := Nullable()
+		assert.NotNil(t, v)
+		assert.Equal(t, "nullable", v.Name())
+		assert.False(t, v.IsType())
+		assert.False(t, v.IsTypeDependent())
+		assert.Empty(t, v.MessagePlaceholders(&ContextV5{}))
+	})
+
+	cases := []struct {
+		value any
+		want  bool
+	}{
+		// Should always return true
+		{value: "string", want: true},
+		{value: "", want: true},
+		{value: 'a', want: true},
+		{value: 2, want: true},
+		{value: 2.5, want: true},
+		{value: []string{"string"}, want: true},
+		{value: map[string]any{"a": 1}, want: true},
+		{value: true, want: true},
+		{value: nil, want: true},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(fmt.Sprintf("Validate_%v_%t", c.value, c.want), func(t *testing.T) {
+			v := Nullable()
+			ctx := &ContextV5{
+				Value: c.value,
+			}
+
+			assert.Equal(t, c.want, v.Validate(ctx))
+		})
+	}
+}

da61792ab2846947f11dbe1a45e1cf66d3511428
SystemGlitch <jeremy.la@outlook.fr>
2023-04-17 11:26:26 +0200

    Validation: test Regex

--- validation/regex_test.go
+++ validation/regex_test.go
@@ -1,19 +1,49 @@
 package validation
 
 import (
+	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRegexCache(t *testing.T) {
-	regex := getRegex(patternDigits)
-	cached, exists := regexCache[patternDigits]
+func TestRegexValidator(t *testing.T) {
+	t.Run("Constructor", func(t *testing.T) {
+		v := Regex(regexp.MustCompile(`^[0-9]+$`))
+		assert.NotNil(t, v)
+		assert.Equal(t, "regex", v.Name())
+		assert.False(t, v.IsType())
+		assert.False(t, v.IsTypeDependent())
+		assert.Equal(t, []string{":regexp", `^[0-9]+$`}, v.MessagePlaceholders(&ContextV5{}))
+	})
 
-	assert.True(t, exists)
-	assert.Equal(t, regex, cached)
-	assert.Same(t, regex, cached)
+	cases := []struct {
+		value any
+		regex *regexp.Regexp
+		want  bool
+	}{
+		{value: "string", want: false, regex: regexp.MustCompile(`^[0-9]+$`)},
+		{value: "0123456789", want: true, regex: regexp.MustCompile(`^[0-9]+$`)},
+		{value: "", want: true, regex: regexp.MustCompile(`^[0-9]*$`)},
+		{value: "", want: false, regex: regexp.MustCompile(`^[0-9]+$`)},
+		{value: 'a', want: false},
+		{value: 2, want: false},
+		{value: 2.5, want: false},
+		{value: []string{"string"}, want: false},
+		{value: []byte{}, want: false},
+		{value: true, want: false},
+		{value: nil, want: false},
+	}
 
-	ClearRegexCache()
-	assert.Empty(t, regexCache)
+	for _, c := range cases {
+		c := c
+		t.Run(fmt.Sprintf("Validate_%v_%t", c.value, c.want), func(t *testing.T) {
+			v := Regex(c.regex)
+			ctx := &ContextV5{
+				Value: c.value,
+			}
+			assert.Equal(t, c.want, v.Validate(ctx))
+		})
+	}
 }

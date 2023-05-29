6be8319d9432b86b18a569c96cc9c47a8514dcf6
SystemGlitch <jeremy.la@outlook.fr>
2023-05-29 14:46:19 +0200

    Test Request

---
+++ request_test.go
@@ -0,0 +1,71 @@
+package goyave
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRequest(t *testing.T) {
+
+	t.Run("NewRequest", func(t *testing.T) {
+		httpReq := httptest.NewRequest(http.MethodGet, "/test", nil)
+		r := NewRequest(httpReq)
+
+		assert.Equal(t, httpReq, r.httpRequest)
+		assert.False(t, r.Now.IsZero())
+		assert.NotNil(t, r.Extra)
+	})
+
+	t.Run("Accessors", func(t *testing.T) {
+		httpReq := httptest.NewRequest(http.MethodPost, "/test", bytes.NewBufferString("hello"))
+		httpReq.Header.Set("X-Test", "value")
+		httpReq.Header.Set("Referer", "referrer")
+		httpReq.Header.Set("User-Agent", "useragent")
+		httpReq.SetBasicAuth("username", "password")
+		httpReq.AddCookie(&http.Cookie{Name: "test-cookie", Value: "value"})
+		r := NewRequest(httpReq)
+
+		assert.Equal(t, httpReq, r.Request())
+		assert.Equal(t, http.MethodPost, r.Method())
+		assert.Equal(t, "HTTP/1.1", r.Protocol())
+		assert.Equal(t, "/test", r.URL().String())
+
+		assert.Equal(t, httpReq.Header, r.Header())
+		assert.Equal(t, "/test", r.URL().String())
+		assert.Equal(t, int64(5), r.ContentLength())
+		assert.Equal(t, "192.0.2.1:1234", r.RemoteAddress())
+
+		cookies := r.Cookies()
+		assert.Equal(t, cookies, r.cookies)
+		assert.Equal(t, []*http.Cookie{{Name: "test-cookie", Value: "value"}}, cookies)
+
+		assert.Equal(t, "referrer", r.Referrer())
+		assert.Equal(t, "useragent", r.UserAgent())
+
+		username, password, ok := r.BasicAuth()
+		assert.Equal(t, "username", username)
+		assert.Equal(t, "password", password)
+		assert.True(t, ok)
+	})
+
+	t.Run("BearerToken", func(t *testing.T) {
+		httpReq := httptest.NewRequest(http.MethodGet, "/test", nil)
+		httpReq.Header.Set("Authorization", "Bearer  token  ")
+		r := NewRequest(httpReq)
+
+		token, ok := r.BearerToken()
+		assert.Equal(t, "token", token)
+		assert.True(t, ok)
+
+		httpReq = httptest.NewRequest(http.MethodGet, "/test", nil)
+		r = NewRequest(httpReq)
+
+		token, ok = r.BearerToken()
+		assert.Empty(t, token)
+		assert.False(t, ok)
+	})
+}

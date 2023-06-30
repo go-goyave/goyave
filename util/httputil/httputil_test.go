98cb3b498de09b0bf6d0d17ff0ffb9b8adf7a714
SystemGlitch <jeremy.la@outlook.fr>
2023-06-30 09:49:32 +0200

    Remove sliceutil package

--- util/httputil/httputil_test.go
+++ util/httputil/httputil_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"goyave.dev/goyave/v5/util/sliceutil"
 )
 
 func TestParseMultiValuesHeader(t *testing.T) {
@@ -14,10 +13,10 @@ func TestParseMultiValuesHeader(t *testing.T) {
 		{Value: "*/*", Priority: 0.8},
 	}
 	result := ParseMultiValuesHeader("text/html;q=0.8,text/*;q=0.8,*/*;q=0.8")
-	assert.True(t, sliceutil.Equal(expected, result))
+	assert.Equal(t, expected, result)
 
 	result = ParseMultiValuesHeader("*/*;q=0.8,text/*;q=0.8,text/html;q=0.8")
-	assert.True(t, sliceutil.Equal(expected, result))
+	assert.Equal(t, expected, result)
 
 	expected = []HeaderValue{
 		{Value: "text/html", Priority: 1},
@@ -25,7 +24,7 @@ func TestParseMultiValuesHeader(t *testing.T) {
 		{Value: "text/*", Priority: 0.5},
 	}
 	result = ParseMultiValuesHeader("text/html,text/*;q=0.5,*/*;q=0.7")
-	assert.True(t, sliceutil.Equal(expected, result))
+	assert.Equal(t, expected, result)
 
 	expected = []HeaderValue{
 		{Value: "fr", Priority: 1},
@@ -36,13 +35,13 @@ func TestParseMultiValuesHeader(t *testing.T) {
 		{Value: "*", Priority: 0.3},
 	}
 	result = ParseMultiValuesHeader("fr , fr-FR;q=0.8, en-US ;q=0.5, *;q=0.3, en-*;q=0.3, en;q=0.3")
-	assert.True(t, sliceutil.Equal(expected, result))
+	assert.Equal(t, expected, result)
 
 	expected = []HeaderValue{{Value: "fr", Priority: 1}}
 	result = ParseMultiValuesHeader("fr")
-	assert.True(t, sliceutil.Equal(expected, result))
+	assert.Equal(t, expected, result)
 
 	expected = []HeaderValue{{Value: "fr", Priority: 0.3}}
 	result = ParseMultiValuesHeader("fr;q=0.3")
-	assert.True(t, sliceutil.Equal(expected, result))
+	assert.Equal(t, expected, result)
 }

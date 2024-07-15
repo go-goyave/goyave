3979f25274dcabdfc506cb6cda94c222b0b2b55c
Aidan Kenney <kenneyaidan@gmail.com>
2024-07-15 15:09:28 -0400

    Run linter

--- database/dialect/clickhouse/clickhouse.go
+++ database/dialect/clickhouse/clickhouse.go
@@ -1,10 +1,10 @@
 package clickhouse
 
 import (
-    "gorm.io/driver/clickhouse"
-    "goyave.dev/goyave/v5/database"
+	"gorm.io/driver/clickhouse"
+	"goyave.dev/goyave/v5/database"
 )
 
 func init() {
-    database.RegisterDialect("clickhouse", "clickhouse://{username}:{password}@{host}:{port}/{name}?{options}", clickhouse.Open)
+	database.RegisterDialect("clickhouse", "clickhouse://{username}:{password}@{host}:{port}/{name}?{options}", clickhouse.Open)
 }

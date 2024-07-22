03f40610c7268cfa0dcd8d3144094ba05e82adf5
Aidan Kenney <kenneyaidan@gmail.com>
2024-07-22 13:06:16 -0400

    Add individual test for BuildQuery with Transform

--- validation/unique_test.go
+++ validation/unique_test.go
@@ -542,9 +542,42 @@ func TestExistsArrayValidator(t *testing.T) {
 			t.Run(c.dialect, func(t *testing.T) {
 				opts := prepareUniqueTest(t)
 				opts.Config.Set("database.connection", c.dialect)
-				v := ExistsArray[int]("models", "name", func(val int) clause.Expr {
-					return gorm.Expr("?", val-1)
+				v := ExistsArray[int]("models", "name", nil)
+				v.init(opts)
+
+				tx, _ := v.buildQuery([]int{2, 7, 6}, true)
+
+				sql := tx.ToSQL(func(tx *gorm.DB) *gorm.DB {
+					return tx
 				})
+				assert.Equal(t, c.expected, sql)
+			})
+		}
+	})
+}
+
+func TestBuildQueryValidatorWithTransform(t *testing.T) {
+	t.Run("buildQuery", func(t *testing.T) {
+		cases := []struct {
+			dialect  string
+			expected string
+		}{
+			{dialect: "sqlite3", expected: "WITH ctx_values(id, i) AS (SELECT * FROM (VALUES (1,0),(6,1),(5,2)) t) SELECT i FROM ctx_values LEFT JOIN `models` ON `models`.`name` = ctx_values.id WHERE `models`.`name` IS  NULL"},
+			{dialect: "mysql", expected: "WITH ctx_values(id, i) AS (SELECT * FROM (VALUES ROW(1,0),ROW(6,1),ROW(5,2)) t) SELECT i FROM ctx_values LEFT JOIN `models` ON `models`.`name` = ctx_values.id WHERE `models`.`name` IS  NULL"},
+			{dialect: "postgres", expected: "WITH ctx_values(id, i) AS (SELECT * FROM (VALUES (1,0::int),(6,1::int),(5,2::int)) t) SELECT i FROM ctx_values LEFT JOIN `models` ON `models`.`name` = ctx_values.id WHERE `models`.`name` IS  NULL"},
+			{dialect: "mssql", expected: "WITH ctx_values(id, i) AS (SELECT * FROM (VALUES (1,0),(6,1),(5,2)) t(id,i)) SELECT i FROM ctx_values LEFT JOIN `models` ON `models`.`name` = ctx_values.id WHERE `models`.`name` IS  NULL"},
+			{dialect: "clickhouse", expected: "WITH ctx_values(id, i) AS (SELECT * FROM (VALUES 'id Int64, i Int64', (1,0),(6,1),(5,2))) SELECT i FROM ctx_values INNER JOIN `models` ON `models`.`name` = ctx_values.id WHERE `models`.`name` IS  NULL"},
+		}
+
+		for _, c := range cases {
+			c := c
+			t.Run(c.dialect, func(t *testing.T) {
+				opts := prepareUniqueTest(t)
+				opts.Config.Set("database.connection", c.dialect)
+				transform := func(val int) clause.Expr {
+					return gorm.Expr("?", val-1)
+				}
+				v := ExistsArray[int]("models", "name", transform)
 				v.init(opts)
 
 				tx, _ := v.buildQuery([]int{2, 7, 6}, true)

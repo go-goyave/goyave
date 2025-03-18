0438b61b0e3ec39635962ed94aabc1df62474634
SystemGlitch <jeremy.la@outlook.fr>
2025-03-18 11:16:47 +0100

    typeutil: handle more cases in Undefined.Scan
    - If the src is also an Undefined (or *Undefined) of the same T, use its Val directly
    - If src is nil, set the Val to T's zero value
    - Prevent nil dereferences if the src is a pointer. Use T's zero value instead and still consider the field as present.
    - Use sql.Scanner implementation as last resort instead of first

--- util/typeutil/undefined_test.go
+++ util/typeutil/undefined_test.go
@@ -32,12 +32,15 @@ func (i testInt64) CopyValue() any {
 }
 
 func (i *testInt64) Scan(src any) error {
-	val, ok := src.(int64)
-	if !ok {
-		return fmt.Errorf("src %#v is not int64", src)
+	switch val := src.(type) {
+	case int64:
+		i.Val = val
+		return nil
+	case testInt64:
+		i.Val = val.Val
+		return nil
 	}
-	i.Val = val
-	return nil
+	return fmt.Errorf("src %#v is not int64", src)
 }
 
 type errValuer struct{}
@@ -188,19 +191,34 @@ func TestUndefined(t *testing.T) {
 			{undefined: &Undefined[string]{}, value: "hello", want: lo.ToPtr(NewUndefined("hello"))},
 			{undefined: &Undefined[string]{}, value: lo.ToPtr("hello"), want: lo.ToPtr(NewUndefined("hello"))},
 			{undefined: &Undefined[testInt64]{}, value: int64(123), want: lo.ToPtr(NewUndefined(testInt64{Val: 123}))},
-			{undefined: &Undefined[testInt64]{}, value: "hello", want: &Undefined[testInt64]{Present: true}, wantErr: fmt.Errorf("src \"hello\" is not int64")},                                      // Error coming from testInt64
+			{undefined: &Undefined[testInt64]{}, value: "hello", want: &Undefined[testInt64]{Present: true}, wantErr: fmt.Errorf("src \"hello\" is not int64")}, // Error coming from testInt64
+			{undefined: &Undefined[testInt64]{}, value: testInt64{Val: 111}, want: &Undefined[testInt64]{Present: true, Val: testInt64{Val: 111}}},
+			{undefined: &Undefined[testInt64]{}, value: Undefined[testInt64]{Val: testInt64{Val: 222}}, want: &Undefined[testInt64]{Present: true, Val: testInt64{Val: 222}}},
+			{undefined: &Undefined[testInt64]{}, value: &Undefined[testInt64]{Val: testInt64{Val: 333}}, want: &Undefined[testInt64]{Present: true, Val: testInt64{Val: 333}}},
 			{undefined: &Undefined[int64]{}, value: "hello", want: &Undefined[int64]{Present: true}, wantErr: fmt.Errorf("typeutil.Undefined: Scan() incompatible types (src: string, dst: int64)")}, // Error coming from Undefined
 			{undefined: &Undefined[int64]{Val: 123}, value: nil, want: &Undefined[int64]{Present: true, Val: 0}},
+			{undefined: &Undefined[int64]{Val: 123}, value: (*int64)(nil), want: &Undefined[int64]{Present: true, Val: 0}},
+			{undefined: &Undefined[int64]{Val: 0}, value: lo.ToPtr(int64(123)), want: &Undefined[int64]{Present: true, Val: 123}},
+			{undefined: &Undefined[int64]{Val: 0}, value: &Undefined[int64]{Present: true, Val: 456}, want: &Undefined[int64]{Present: true, Val: 456}},
+			{undefined: &Undefined[int64]{Val: 0}, value: Undefined[int64]{Present: true, Val: 789}, want: &Undefined[int64]{Present: true, Val: 789}},
+			{undefined: &Undefined[int64]{Val: 34}, value: &Undefined[int64]{}, want: &Undefined[int64]{Present: true}},
+			{undefined: &Undefined[int64]{Val: 56}, value: Undefined[int64]{}, want: &Undefined[int64]{Present: true}},
+			{undefined: &Undefined[int64]{Val: 12}, value: (*Undefined[int64])(nil), want: &Undefined[int64]{Present: true, Val: 0}},
 			{undefined: &Undefined[*int64]{Val: lo.ToPtr(int64(123))}, value: nil, want: &Undefined[*int64]{Present: true, Val: nil}},
+			{undefined: &Undefined[*int64]{Val: lo.ToPtr(int64(123))}, value: (*int64)(nil), want: &Undefined[*int64]{Present: true, Val: nil}},
 		}
 
 		for _, c := range cases {
-			err := c.undefined.Scan(c.value)
-			if c.wantErr != nil {
-				require.ErrorContains(t, err, c.wantErr.Error())
-			}
+			t.Run("", func(t *testing.T) {
+				err := c.undefined.Scan(c.value)
+				if c.wantErr != nil {
+					require.ErrorContains(t, err, c.wantErr.Error())
+				} else {
+					require.NoError(t, err)
+				}
 
-			assert.Equal(t, c.want, c.undefined)
+				assert.Equal(t, c.want, c.undefined)
+			})
 		}
 	})
 
